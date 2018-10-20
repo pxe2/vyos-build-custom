0a8062c33f74494deafb9708718c8d7cdce43165
Peter Pouliot <peter@pouliot.net>
2018-10-20 15:13:03 -0400

    build launch script

---
+++ scripts/build-it.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+git clone https://github.com/vyos/vyos-build /usr/src/vyos-build
+cd /usr/src/vyos-build 
+./configure 
+make iso'
+cp /usr/src/vyos-build/build/*.iso /root/images
